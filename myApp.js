062044338bd7bd357f282e095db49c1ecd4c0d45
Ivan Grasso <grassoivan@gmail.com>
2021-05-23 20:29:36 -0300

    change order

--- myApp.js
+++ myApp.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
 console.log("Hello World")
+app.use("/public", express.static(__dirname + "/public"));
 app.get('/', function(req, res) {
  var absolutePath = __dirname + '/views/index.html'
  res.sendFile(absolutePath)
 });
-app.use("/public", express.static(__dirname + "/public"));
-
 
 
 
