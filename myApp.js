e8bfb649aae9a9065fde4d7c971af482f67ab020
Ivan Grasso <ivan.grasso@mercadolibre.com>
2021-05-23 23:59:16 +0000

    fix! now 4 real

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 console.log("Hello World")
-app.use('*/public', express.static(__dirname + '/public'));
+app.use('/public/', express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
  var absolutePath = __dirname + '/views/index.html'
  res.sendFile(absolutePath)
