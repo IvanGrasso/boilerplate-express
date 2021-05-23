9669e721d2af49628e349770bc42767509c6b69d
Ivan Grasso <grassoivan@gmail.com>
2021-05-22 23:56:37 -0300

    serve index.html

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 console.log("Hello World")
 app.get('/', function(req, res) {
- res.send('Hello Express');
+ var absolutePath = __dirname + '/views/index.html'
+ res.sendFile(absolutePath)
 });
 
 
