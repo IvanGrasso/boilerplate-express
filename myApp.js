cf93b28e4e6622a11eda3062f0627a2716ad6954
Ivan Grasso <grassoivan@gmail.com>
2021-05-24 00:02:42 -0300

    echo server

--- myApp.js
+++ myApp.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+// Log to the console
 console.log("Hello World");
 
 // Middleware
 
+// Root-level request logger
 app.use(function(req, res, next) {
  console.log(req.method + " " + req.path + " - " + req.ip);
  next();
@@ -13,15 +15,18 @@ app.use(function(req, res, next) {
 
 // Routing
 
+// Serve HTML file
 app.get('/', function(req, res) {
- var absolutePath = __dirname + '/views/index.html'
- res.sendFile(absolutePath)
+ var absolutePath = __dirname + '/views/index.html';
+ res.sendFile(absolutePath);
 });
 
+// Serve static assets
 app.use('/public/', express.static(__dirname + '/public'));
 
 // API
 
+// Using .env file
 app.get('/json', function(req, res) {
  if (process.env.MESSAGE_STYLE === "uppercase") {
   res.json({"message": "HELLO JSON"});
@@ -30,12 +35,18 @@ app.get('/json', function(req, res) {
  }
 });
 
+// Time server
 app.get('/now', function (req, res, next) {
  req.time = new Date().toString();
  next();
 }, function (req, res) {
- res.json({time: req.time})
-})
+ res.json({time: req.time});
+});
+
+// Get Route Parameter Input from the Client
+app.get('/:word/echo', function (req, res) {
+ res.json({"echo": req.params.word})
+});
 
 module.exports = app;
 
