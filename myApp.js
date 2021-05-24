952f51e639513b8f82c9df3bccce994f15ae7690
Ivan Grasso <grassoivan@gmail.com>
2021-05-23 23:50:16 -0300

    time server

--- myApp.js
+++ myApp.js
@@ -1,16 +1,27 @@
-require('dotenv').config()
+require('dotenv').config();
 var express = require('express');
 var app = express();
-console.log("Hello World")
+
+console.log("Hello World");
+
+// Middleware
+
 app.use(function(req, res, next) {
  console.log(req.method + " " + req.path + " - " + req.ip);
  next();
 });
-app.use('/public/', express.static(__dirname + '/public'));
+
+// Routing
+
 app.get('/', function(req, res) {
  var absolutePath = __dirname + '/views/index.html'
  res.sendFile(absolutePath)
 });
+
+app.use('/public/', express.static(__dirname + '/public'));
+
+// API
+
 app.get('/json', function(req, res) {
  if (process.env.MESSAGE_STYLE === "uppercase") {
   res.json({"message": "HELLO JSON"});
@@ -18,6 +29,14 @@ app.get('/json', function(req, res) {
   res.json({"message": "Hello json"});
  }
 });
+
+app.get('/now', function (req, res, next) {
+ req.time = new Date().toString();
+ next();
+}, function (req, res) {
+ res.json({time: req.time})
+})
+
 module.exports = app;
 
 
