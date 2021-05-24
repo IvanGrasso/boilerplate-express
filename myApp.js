84fb4c07a75886f2eb94391034629e17d8ce6b8b
Ivan Grasso <grassoivan@gmail.com>
2021-05-23 21:06:54 -0300

    serve json

--- myApp.js
+++ myApp.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 console.log("Hello World")
-app.use('/public', express.static(__dirname + '/public'));
+app.use('*/public', express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
  var absolutePath = __dirname + '/views/index.html'
  res.sendFile(absolutePath)
+});
+app.get('/json', function(req, res) {
+ var absolutePath = __dirname + '/views/index.html'
+ res.json({"message": "Hello json"})
 });
 
 
