55b88d5cb670c38d16cbad1d2292072cf6c14f0b
Ivan Grasso <grassoivan@gmail.com>
2021-05-23 22:02:20 -0300

    environment variable

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var express = require('express');
 var app = express();
 console.log("Hello World")
@@ -7,11 +8,13 @@ app.get('/', function(req, res) {
  res.sendFile(absolutePath)
 });
 app.get('/json', function(req, res) {
- var absolutePath = __dirname + '/views/index.html'
- res.json({"message": "Hello json"})
+ if (process.env.MESSAGE_STYLE === "uppercase") {
+  res.json({"message": "HELLO JSON"});
+ } else {
+  res.json({"message": "Hello json"});
+ }
 });
-
-
+module.exports = app;
 
 
 
